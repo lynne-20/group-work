4d502ef32afde788f5ce794ce75f35fa9928755c
lynne-20 <147532078+lynne-20@users.noreply.github.com>
2023-10-10 18:55:18 +0300

    Add files via upload

---
+++ task1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+#include "lexer_header.h"
+
+
+#undef yywrap
+
+extern "C" int yywrap() {
+    return 1; 
+}
+
+int main() {
+    yylex();
+    return 0;
+}
